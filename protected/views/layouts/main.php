1473601838fd82ad915d7f29a510bceeb8935fab
Supasate Choochaisri <supasate.c@gmail.com>
2012-06-03 12:20:09 +0700

    Change the footer.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -47,9 +47,8 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		Copyright &copy; <?php echo date('Y'); ?> Zelfer.com<br/>
 		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
 
 </div><!-- page -->
