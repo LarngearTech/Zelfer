786eac2aba980a81e49cd8ae8777f56a8eb3280e
Supasate Choochaisri <supasate.c@gmail.com>
2012-06-03 18:31:31 +0700

    Use CHtml to format HTML elements.

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -12,31 +12,30 @@
 
 // category sections
 foreach ($categories as $category) {
+	// display category name
 	echo '
-	<br />
-<div class="page-header">
-	<h1>'.$category->name.'</h1>
-</div>';
-	echo '
-<ul class="thumbnails">';
+		<br />
+		<div class="page-header">
+			<h1>'.CHtml::encode($category->name).'</h1>
+		</div>';
 
 	// display courses of each category
+	echo CHtml::openTag('ul', array('class' => 'thumbnails'));
 	foreach ($courses_in_categories[$category->id] as $course) {
-		echo '
-	<li class="span3">';
+		echo CHtml::openTag('li', array('class' => 'span3'));
 		echo CHtml::link(
 			// image to be linked
-			CHtml::image($course->thumbnailUrl, 'Course thumbnail: '.$course->name),
+			CHtml::image($course->thumbnailUrl, CHtml::encode('Course thumbnail: '.$course->name)),
 			// url to course/view?id=course_id
 			array('course/view', 'id' => $course->id),
 			// class for tag a href
 			array('class' => 'thumbnail')
 		);
 		echo '
-		<h5>'.$course->name.'</h5>
-		<p>'.$course->description.'</p>
-	</li>';
+		<h5>'.CHtml::encode($course->name).'</h5>
+		<p>'.CHtml::encode($course->description).'</p>';
+		echo CHtml::closeTag('li');
 	}
-	echo '</ul>';
+	echo CHtml::closeTag('ul');
 }
 ?>
