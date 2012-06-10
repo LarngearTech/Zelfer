114b849fd46a61e95fb9b36aa7a313acb3d49c34
Supasate Choochaisri <supasate@larngeartech.com>
2012-06-11 00:10:40 +0700

    Prepare for url rewrite (but it's not working yet)

--- protected/config/main.php
+++ protected/config/main.php
@@ -43,16 +43,15 @@ return array(
 			'class' => 'WebUser',
 		),
 		// uncomment the following to enable URLs in path-format
-		/*
 		'urlManager'=>array(
 			'urlFormat'=>'path',
+			'showScriptName' => false,
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
 		// uncomment the following to use a SQLite database
 		/*
 		'db'=>array(
