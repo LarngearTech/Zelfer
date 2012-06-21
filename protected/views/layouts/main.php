711cae25799e0c1ebd12bc108c033d4cf64cbae1
Supasate Choochaisri <supasate@larngeartech.com>
2012-06-21 09:50:35 +0700

    Remove Blueprint CSS and use Bootstrap CSS.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -4,15 +4,8 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
 
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
-
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+	<!-- bootstrap CSS framework -->
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap.css" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
@@ -21,21 +14,25 @@
 
 <div class="container" id="page">
 
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
-
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items' => array(
-				array('label' => Yii::t('site', 'Home'), 'url' => array('/site/index')),
-				array('label' => Yii::t('site', 'About'), 'url' => array('/site/page', 'view'=>'about')),
-				array('label' => Yii::t('site', 'Contact'), 'url' => array('/site/contact')),
-				array('label' => Yii::t('site', 'My Courses'), 'url' => array('/user/mycourses'), 'visible' => !Yii::app()->user->isGuest),
-				array('label' => Yii::t('site', 'Login'), 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
-				array('label' => Yii::t('site', 'Logout').' ('.Yii::app()->user->fullname.')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
-			),
-		)); ?>
+	<div id="mainmenu" class="navbar">
+		<div class="navbar-inner">
+			<div class="container">
+				<a class="brand" href="#"><?php echo CHtml::encode(Yii::app()->name); ?></a>
+				<?php $this->widget('zii.widgets.CMenu',array(
+					'items' => array(
+						array('label' => Yii::t('site', 'Home'), 'url' => array('/site/index')),
+						array('label' => Yii::t('site', 'About'), 'url' => array('/site/page', 'view'=>'about')),
+						array('label' => Yii::t('site', 'Contact'), 'url' => array('/site/contact')),
+						array('label' => Yii::t('site', 'My Courses'), 'url' => array('/user/mycourses'), 'visible' => !Yii::app()->user->isGuest),
+						array('label' => Yii::t('site', 'Login'), 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
+						array('label' => Yii::t('site', 'Logout').' ('.Yii::app()->user->fullname.')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
+					),
+					'htmlOptions' => array(
+						'class' => 'nav',
+					),
+				)); ?>
+			</div>
+		</div>
 	</div><!-- mainmenu -->
 	<?php if(isset($this->breadcrumbs)):?>
 		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
