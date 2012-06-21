9a05be2f8ebda4e3afd3c2ed3e931d6714d29589
Supasate Choochaisri <supasate@larngeartech.com>
2012-06-21 17:06:13 +0700

    Modal can submit form.

--- protected/views/course/view.php
+++ protected/views/course/view.php
@@ -25,20 +25,29 @@ $this->breadcrumbs = array(
 // If a user is a guest, show 'Take Course' and proceed the sign up.
 if (Yii::app()->user->isGuest)
 {
+	echo "<script type='text/javascript'>
+	$(document).ready(function() {
+    		$('#signup-btn').click(function(e) {
+        		e.preventDefault();
+        		$('#signUpModal').modal('hide');
+        		$('form').submit();
+		})
+    });
+	</script>";
 	$this->beginWidget('EBootstrapModal', array(
 		'id' => 'signUpModal',
 		'show' => false,
 		'header' => Yii::t('site', 'To take this course, please sign up.'),
 		'footer' => array(
 			EBootstrap::ibutton(
-                		Yii::t('site', 'Take course'),
-                		'index.php?r=course/inclass&id='.$model->id,
+                		Yii::t('site', 'Sign Up'),
+                		'#',
                 		'primary',
                 		'large',
                 		false,
                 		'',
                 		false,
-                		array()
+                		array('id' => 'signup-btn')
         		),
 			EBootstrap::ibutton(
                 		Yii::t('site', 'Cancel'),
@@ -50,11 +59,10 @@ if (Yii::app()->user->isGuest)
                 		false,
                 		array('data-dismiss' => 'modal')
 			),
-			'</form>',
 		),
-	)); ?>
- 
-	<?php $userModel = new User; 
+	)); 
+	// Modal content
+	$userModel = new User; 
 	$form = $this->beginWidget('EBootstrapActiveForm', array(
 		'id' => 'user-form',
 		'enableClientValidation' => true,
@@ -64,41 +72,36 @@ if (Yii::app()->user->isGuest)
 		'action' => 'index.php?r=user/create',
 	));
 	?>
-		<div class="modal-body">
-			<p>
-				<?php echo $form->errorSummary($userModel); ?>
-				<div>
-					<?php echo $form->labelEx($userModel,'fullname'); ?>
-					<?php echo $form->textField($userModel,'fullname',array('size'=>60,'maxlength'=>128)); ?>
-					<?php echo $form->error($userModel,'fullname'); ?>
-				</div>
+	<?php echo $form->errorSummary($userModel); ?>
+	<div>
+		<?php echo $form->labelEx($userModel,'fullname'); ?>
+		<?php echo $form->textField($userModel,'fullname',array('size'=>60,'maxlength'=>128)); ?>
+		<?php echo $form->error($userModel,'fullname'); ?>
+	</div>
 
-				<div>
-					<?php echo $form->labelEx($userModel,'email'); ?>
-					<?php echo $form->textField($userModel,'email',array('size'=>60,'maxlength'=>128)); ?>
-					<?php echo $form->error($userModel,'email'); ?>
-				</div>
+	<div>
+		<?php echo $form->labelEx($userModel,'email'); ?>
+		<?php echo $form->textField($userModel,'email',array('size'=>60,'maxlength'=>128)); ?>
+		<?php echo $form->error($userModel,'email'); ?>
+	</div>
 
-				<div>
-					<?php echo $form->labelEx($userModel,'password'); ?>
-					<?php echo $form->passwordField($userModel,'password',array('size'=>60,'maxlength'=>128)); ?>
-					<?php echo $form->error($userModel,'password'); ?>
-				</div>
+	<div>
+		<?php echo $form->labelEx($userModel,'password'); ?>
+		<?php echo $form->passwordField($userModel,'password',array('size'=>60,'maxlength'=>128)); ?>
+		<?php echo $form->error($userModel,'password'); ?>
+	</div>
 
-				<div>
-					<?php echo $form->labelEx($userModel,'repeat_password'); ?>
-					<?php echo $form->passwordField($userModel,'repeat_password',array('size'=>60,'maxlength'=>128)); ?>
-					<?php echo $form->error($userModel,'repeat_password'); ?>
-				</div>
-				<?php echo CHtml::hiddenField('returnUrl', $model->getInClassUrl());?>
+	<div>
+		<?php echo $form->labelEx($userModel,'repeat_password'); ?>
+		<?php echo $form->passwordField($userModel,'repeat_password',array('size'=>60,'maxlength'=>128)); ?>
+		<?php echo $form->error($userModel,'repeat_password'); ?>
+	</div>
+	<?php echo CHtml::hiddenField('returnUrl', $model->getInClassUrl());?>
 				
-			</p>
-		</div>
- 
-		<?php 
-		$this->endWidget(); // end ActiveForm widget
+	<?php 
+	$this->endWidget(); // end ActiveForm widget
 
-	$this->endWidget(); // end modal widget
+	$this->endWidget(); // end Modal widget
 
 	// Take course button
 	$this->widget('EBootstrapModalTrigger', array(
