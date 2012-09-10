7b3daf1786eacc160ae46d233ecfde87d782ec99
Supasate Choochaisri <supasate@larngeartech.com>
2012-09-10 13:29:45 +0700

    Add LoginSignUpModal widget

---
+++ protected/widgets/LoginSignupModal/LoginSignupModal.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Log-in and Sign-up Modal widget
+ */
+class LoginSignupModal extends CWidget
+{
+
+	public function init()
+	{
+
+	}
+
+	public function run()
+	{
+		$this->render('loginSignupModal');
+	}
+}
+
+?>
