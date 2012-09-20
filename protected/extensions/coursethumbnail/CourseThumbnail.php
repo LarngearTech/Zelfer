e16d8a49609f6fbd4f7781fb4ba71ff27cd1c6fe
topscores <topscores@topscores-HP-Pavilion-dv2000-RE054PA-AKL.(none)>
2012-09-21 00:12:47 +0700

    modify CourseThumbnail widget

--- protected/extensions/coursethumbnail/CourseThumbnail.php
+++ protected/extensions/coursethumbnail/CourseThumbnail.php
@@ -10,7 +10,7 @@ class CourseThumbnail extends CWidget{
 	function run(){
 		// Get assets dir
 		$baseDir = dirname(__FILE__);
-		$assets = Yii::app()->getAssetManager()->publish($baseDir.DIRECTORY_SEPARATOR.'assets');
+		$assets = Yii::app()->getAssetManager()->publish($baseDir.DIRECTORY_SEPARATOR.'assets', false, -1, YII_DEBUG);
 
 		// Publish required assets
 		$cs = Yii::app()->getClientScript();
@@ -24,6 +24,7 @@ class CourseThumbnail extends CWidget{
 		// User specified course. Give priority to directly specified paramenter's value first, find from $course if isn't given.
 		else {
 			$this->thumbnailUrl		= ($this->thumbnailUrl)?$this->thumbnailUrl:$this->course->thumbnailUrl;
+			//$this->thumbnailUrl		= file_exists(Yii::app()->basePath.'/../..'.$this->thumbnailUrl)?$this->thumbnailUrl:$assets.'/img/default.jpg';
 			$this->courseName		= ($this->courseName)?$this->courseName:$this->course->name;
 			$this->courseShortDescription	= ($this->courseShortDescription)?$this->courseShortDescription:$this->course->short_description;
 			$this->courseUrl		= ($this->courseUrl)?$this->courseUrl:Yii::app()->createUrl('course/view', array('id'=>$this->course->id));
--- protected/extensions/coursethumbnail/views/coursethumbnail.php
+++ protected/extensions/coursethumbnail/views/coursethumbnail.php
@@ -1,14 +1,9 @@
-<?php
-echo CHtml::link(
-// image to be linked
-CHtml::image($thumbnailUrl, CHtml::encode('Course thumbnail: '.$courseName), array(
-'height' => '180',
-)).
-'<h5>'.CHtml::encode($courseName).'</h5>
-<p>'.CHtml::encode($courseShortDescription).'</p>',
-// url to course/view?id=course_id
-$courseUrl,
-// class for tag a href
-array('class' => $css)
-);
-?>
+<div class=<?php echo $css; ?>>
+	<div id='thumbnail-img-container'>
+		<img src=<?php echo $thumbnailUrl; ?> alt='Course thumbnail: '.<?php echo CHtml::encode($courseName); ?>/>
+	</div>
+	<div id='thumbnail-caption-container'>
+		<h5><?php echo CHtml::encode($courseName) ?></h5>
+		<p><?php echo CHtml::encode($courseShortDescription) ?></p>
+	</div>
+</div>
