aa0685d4d022d87cd85726adfb5d818a7ad65b75
Supasate Choochaisri <supasate@larngeartech.com>
2012-10-01 11:54:25 +0700

    Add container wrapper to myCourse and reindent code.

--- protected/views/course/myCourse.php
+++ protected/views/course/myCourse.php
@@ -2,11 +2,11 @@
 Yii::app()->clientScript->registerScriptFile(
 	Yii::app()->baseUrl.'/js/bootstrap.min.js'
 );?>
-
-<h1>Courses|Take</h1>
+<div class="container">
+	<h1>Courses|Take</h1>
 	<ul class='thumbnails'>
-	<?php
-		foreach ($userModel->takeCourses as $course){
+		<?php
+		foreach ($userModel->takeCourses as $course) {
 			//print_r($course);
 			echo CHtml::openTag('li', array('class' => 'span3'));
  			$this->widget('CourseThumbnail', array(
@@ -18,26 +18,27 @@ Yii::app()->clientScript->registerScriptFile(
 		$this->widget('AddCourseThumbnail', array(
 							'redirectUrl'=>$this->createUrl('site/index'),
 							'caption'=>'Browse Course'
-			));
+		));
 		echo CHtml::closeTag('li');
-	?>
+		?>
 	</ul>
-<h1>Courses|Teach</h1>
+	<h1>Courses|Teach</h1>
 	<ul class='thumbnails'>
-	<?php	
-		foreach ($userModel->teachCourses as $course){
+		<?php	
+		foreach ($userModel->teachCourses as $course) {
 			//print_r($course);
 			echo CHtml::openTag('li', array('class' => 'span3'));
 			$this->widget('CourseThumbnail', array(
 							'course'=>$course,
 			));
 			echo CHtml::closeTag('li');
-                }
-                echo CHtml::openTag('li', array('class' => 'span3'));
-                $this->widget('AddCourseThumbnail', array(
-                                                        'redirectUrl'=>$this->createUrl('course/create'),
-                                                        'caption'=>'Create Course'
-                        ));
-                echo CHtml::closeTag('li');
-        ?>
-        </ul>
+		}
+		echo CHtml::openTag('li', array('class' => 'span3'));
+		$this->widget('AddCourseThumbnail', array(
+	 					 'redirectUrl'=>$this->createUrl('course/create'),
+	 					 'caption'=>'Create Course'
+		));
+	 	echo CHtml::closeTag('li');
+		?>
+	</ul>
+</div><!-- /container -->
