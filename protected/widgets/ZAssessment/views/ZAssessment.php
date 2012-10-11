438ba76be019de82efaa6e89dbf527320ad7e0c8
Supasate Choochaisri <supasate@larngeartech.com>
2012-10-11 17:26:29 +0700

    Handle lecture id and user id in assessment

--- protected/widgets/ZAssessment/views/ZAssessment.php
+++ protected/widgets/ZAssessment/views/ZAssessment.php
@@ -1,11 +1,13 @@
 <div class="assessments">
+<?php echo CHtml::beginForm(); ?>
 	<h2><?php echo $headline; ?></h2>
 	<h4><?php echo $description; ?></h4>
 	<?php foreach ($items as $item): ?>
-	<?php $this->widget('ZAssessmentItem', array(
-		'id' => $item['id'],
-		'itemPath' => $item['itemPath'],
-	));?>
+		<?php $this->widget('ZAssessmentItem', array(
+			'id' => $item['id'],
+			'itemPath' => $item['itemPath'],
+		));?>
 	<?php endforeach; ?>
+	<?php echo CHtml::submitButton(Yii::t('site', 'Submit'), array('class' => 'btn btn-primary')); ?>
+<?php echo CHtml::endForm(); ?>
 </div><!-- /assessments -->
-
