722384b135b589b048ce45b36da47f5921b0750d
Supasate Choochaisri <supasate@larngeartech.com>
2012-10-17 10:37:44 +0700

    Add background image in the header section

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,16 +1,20 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
-<div class="container">
-	<?php if (Yii::app()->user->isGuest): ?>
-	<div class="row">
-		<div class="span9">
-			<img src="<?php echo Yii::app()->baseUrl.'/images/banner/6.jpg';?>" />
-		</div><!-- /span9 -->
-		<div class="span3">
-			<?php $this->widget('ZLogInSignUpFlipper');?>
-		</div><!-- /span3 -->
-	</div><!-- /row -->
-	<?php endif; ?>
-	<div class="course-list">
+<div class="header-section">
+	<div class="container">
+		<?php if (Yii::app()->user->isGuest): ?>
+		<div class="row">
+			<div class="span9">
+				<img src="<?php echo Yii::app()->baseUrl.'/images/banner/6.jpg';?>" />
+			</div><!-- /span9 -->
+			<div class="span3">
+				<?php $this->widget('ZLogInSignUpFlipper');?>
+			</div><!-- /span3 -->
+		</div><!-- /row -->
+		<?php endif; ?>
+	</div><!-- /container -->
+</div><!-- /header-section -->
+<div class="course-list-section">
+	<div class="container">
 		<?php foreach ($categories as $category): ?>
 			<div class="course-category">
 				<h1><?php echo CHtml::encode($category->name); ?></h1>
@@ -27,5 +31,5 @@
 				echo CHtml::closeTag('ul'); ?>
 			</div><!-- /course-category -->
 		<?php endforeach; ?>
-	</div><!-- /course-list -->
-</div><!-- /container -->
+	</div><!-- /container -->
+</div><!-- /course-list-section -->
