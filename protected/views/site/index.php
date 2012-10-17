5e1844921f60895597393294dda4763f20ecd7b8
Supasate Choochaisri <supasate@larngeartech.com>
2012-10-17 10:07:49 +0700

    Adapt margins between course categories.

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -12,18 +12,20 @@
 	<?php endif; ?>
 	<div class="course-list">
 		<?php foreach ($categories as $category): ?>
-			<h1><?php echo CHtml::encode($category->name); ?></h1>
-			<?php // display courses of each category ?>
-			<?php echo CHtml::openTag('ul', array('class' => 'thumbnails'));
-			foreach ($courses_in_categories[$category->id] as $course)
-			{
-				echo CHtml::openTag('li', array('class' => 'span4'));
-				$this->widget('CourseThumbnail', array(
-								'course'=>$course,
-				));
-				echo CHtml::closeTag('li');
-			}
-			echo CHtml::closeTag('ul'); ?>
+			<div class="course-category">
+				<h1><?php echo CHtml::encode($category->name); ?></h1>
+				<?php // display courses of each category ?>
+				<?php echo CHtml::openTag('ul', array('class' => 'thumbnails'));
+				foreach ($courses_in_categories[$category->id] as $course)
+				{
+					echo CHtml::openTag('li', array('class' => 'span4'));
+					$this->widget('CourseThumbnail', array(
+									'course'=>$course,
+					));
+					echo CHtml::closeTag('li');
+				}
+				echo CHtml::closeTag('ul'); ?>
+			</div><!-- /course-category -->
 		<?php endforeach; ?>
 	</div><!-- /course-list -->
 </div><!-- /container -->
