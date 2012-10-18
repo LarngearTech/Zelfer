79cff3a195f39fc2a46822f3da0e3f7bdb331578
root <root@topscores-HP-Pavilion-dv2000-RE054PA-AKL.(none)>
2012-10-18 17:41:45 +0700

    Add ContentList widget to course view page

--- protected/widgets/ContentList/views/contentList.php
+++ protected/widgets/ContentList/views/contentList.php
@@ -1,18 +1,63 @@
 <?php
-	$i=0;
-	$j=0;
+	$hasHeading=false;
+	$chapterId=1;
+
 	foreach($contents as $content)
 	{
 		if ($content->parent_id == 0)
 		{
-			echo 'chapter '.$i.':'.$content->name.'</br>';
-			$i++;
-			$j=0;
+			// Add closing tag
+			if ($hasHeading){
+				addChapterEnding();
+				$hasHeading=false;
+			}
+			addChapterHeading($chapterId, $content);
+			$hasHeading=true;
+			
+			$chapterId++;
 		}
 		else
 		{
-			echo 'lecture '.$j.':'.$content->name.'</br>';
-			$j++;
+			addChapterContent($content);
 		}
 	}
+	addChapterEnding();
+?>
+
+<?php
+function addChapterHeading($chapterId, $content)
+{
+?>
+<div class="accordion-group">
+	<div class="accordion-heading">
+		<a class="accordion-toggle" data-toggle="collapse" data-parent="#chapter-accordion" href="#chapter<?php echo $chapterId; ?>-collapse">
+			<?php echo Yii::t('site', 'Chapter').' '.$chapterId.' '.CHtml::encode($content->name); ?>
+		</a>
+	</div><!-- heading -->
+	<div id="chapter<?php echo $chapterId?>-collapse" class="accordion-body collapse in">
+		<div class="accordion-inner">
+			<ul>
+<?php
+}
+?>
+
+<?php
+function addChapterEnding()
+{
+?>
+			</ul>
+		</div><!-- inner -->
+	</div><!-- body -->
+</div><!-- group -->
+<?php
+}
+?>
+
+<?php
+function addChapterContent($content)
+{
+?>
+	<li><?php echo CHtml::encode($content->name); ?></li>
+<?php
+}
 ?>
