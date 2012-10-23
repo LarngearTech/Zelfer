58ba689fbee5110791b94329e213aa3cffe2d93f
Supasate Choochaisri <supasate@larngeartech.com>
2012-10-24 00:46:33 +0700

    1. Add thumbnail caption padding. 2. Add stylesheet for introvideouploader.

--- protected/widgets/IntroVideo/IntroVideoUploader.php
+++ protected/widgets/IntroVideo/IntroVideoUploader.php
@@ -1,8 +1,21 @@
 <?php
 class IntroVideoUploader extends CWidget{
+
 	public $course;
 
-	function run(){
+	function init()
+	{
+		// Get assets dir
+        $baseDir = dirname(__FILE__);
+        $assets = Yii::app()->getAssetManager()->publish($baseDir.DIRECTORY_SEPARATOR.'assets', false, -1, YII_DEBUG);
+
+		// Publish required assets
+		$cs = Yii::app()->getClientScript();
+		$cs->registerCssFile($assets.DIRECTORY_SEPARATOR.'css'.DIRECTORY_SEPARATOR.'style.css');
+	}
+
+	function run()
+	{
 		if ($this->course){
 			// Render widget
 			echo $this->render('introvideouploader', array(
