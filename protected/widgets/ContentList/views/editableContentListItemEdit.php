5b3db73bf45afb5d8885fd4e966acbce91d833ff
Supasate Choochaisri <supasate@larngeartech.com>
2012-11-08 22:10:02 +0700

    Move edit-panel in the editing mode to the same position in the normal mode.

--- protected/widgets/ContentList/views/editableContentListItemEdit.php
+++ protected/widgets/ContentList/views/editableContentListItemEdit.php
@@ -3,6 +3,16 @@
 	<span id="content-name-<?php echo $content->id; ?>" class="content-name">
 		<input id="txt-content-name-<?php echo $content->id; ?>" type="text" value="<?php echo $content->name; ?>"/>
 	</span>
+	<div class="edit-panel">
+		<a class="btn commit-content" onclick="js:commitContent(
+			<?php echo $content->id; ?>,
+			'<?php echo $contentPrefix; ?>'
+			)" title="commit change">
+			<i class="icon-ok"></i>
+		</a>
+		<a class="btn cancel-edit-edit" onclick="js:cancelEditContent(<?php echo $content->id; ?>, '<?php echo $contentPrefix; ?>')" title="reset"><i class="icon-remove"></i></a>
+		<a class="btn delete-content" onclick="js:deleteContent(<?php echo $content->id; ?>)" title="delete content"><i class="icon-trash"></i></a>
+	</div>
 	<?php
 	if (!$content->isChapter())
 	{
@@ -14,14 +24,4 @@
 <?php
 	}
 	?>
-	<div class="edit-panel">
-		<a class="btn commit-content" onclick="js:commitContent(
-			<?php echo $content->id; ?>,
-			'<?php echo $contentPrefix; ?>'
-			)" title="commit change">
-			<i class="icon-ok"></i>
-		</a>
-		<a class="btn cancel-edit-edit" onclick="js:cancelEditContent(<?php echo $content->id; ?>, '<?php echo $contentPrefix; ?>')" title="reset"><i class="icon-remove"></i></a>
-		<a class="btn delete-content" onclick="js:deleteContent(<?php echo $content->id; ?>)" title="delete content"><i class="icon-trash"></i></a>
-	</div>
 </div>
