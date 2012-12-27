12d088f7e9dfbea42a1459c1e0746dc0e18e390e
root <root@topscores-wt1.(none)>
2012-12-27 11:47:20 +0700

    add index-admin page

---
+++ protected/views/site/index-admin.php
@@ -0,0 +1,8 @@
+<?php $this->pageTitle=Yii::app()->name; ?>
+<?php if (Yii::app()->user->isGuest): ?>
+<?php endif; ?>
+<div class="course-list-section">
+	<div class="container">
+		<h1>I'm admin</h1>
+	</div><!-- /container -->
+</div><!-- /course-list-section -->
