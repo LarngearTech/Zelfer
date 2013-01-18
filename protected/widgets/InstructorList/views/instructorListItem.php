0f6c9daa7bc642e1d660ca3ec46bb105a1de82a7
Supasate Choochaisri <supasate@larngeartech.com>
2013-01-18 09:57:29 +0700

    Make instrutor item look better.

--- protected/widgets/InstructorList/views/instructorListItem.php
+++ protected/widgets/InstructorList/views/instructorListItem.php
@@ -1,10 +1,12 @@
 <div class="instructor">
-	<div class="instructor-image">
-		<?php echo CHtml::image($profileImageUrl, 'Image of '.$instructor->fullname); ?>
-	</div>
-	<div class="instructor-detail">
+	<div class="instructor-header">
+		<div class="instructor-image">
+			<?php echo CHtml::image($profileImageUrl, 'Image of '.$instructor->fullname, array('class' => '')); ?>
+		</div>
 		<h2><?php echo $instructor->fullname; ?></h3>
 		<h4><?php echo $instructor->career; ?></h4>
+	</div>
+	<div class="instructor-detail">
 		<p><?php echo $instructor->description; ?></p>
 	</div>
 </div>
