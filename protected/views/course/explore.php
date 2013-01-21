004b9eec6fc9248e43ff20d6cd5b6657dca181b7
Supasate Choochaisri <supasate@larngeartech.com>
2013-01-21 18:00:56 +0700

    Add course explore page.

---
+++ protected/views/course/explore.php
@@ -0,0 +1,4 @@
+<?php $this->renderPartial('_courseList', array(
+	'categories' => $categories,
+	'courses_in_categories' => $courses_in_categories,
+));?>
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -26,23 +26,7 @@
 	</div><!-- /container -->
 </div><!-- /header-section -->
 <?php endif; ?>
-<div class="course-list-section">
-	<div class="container">
-		<?php foreach ($categories as $category): ?>
-			<div class="course-category">
-				<h1><?php echo CHtml::encode($category->name); ?></h1>
-				<?php // display courses of each category ?>
-				<?php echo CHtml::openTag('ul', array('class' => 'thumbnails'));
-				foreach ($courses_in_categories[$category->id] as $course)
-				{
-					echo CHtml::openTag('li', array('class' => 'span4'));
-					$this->widget('CourseThumbnail', array(
-									'course'=>$course,
-					));
-					echo CHtml::closeTag('li');
-				}
-				echo CHtml::closeTag('ul'); ?>
-			</div><!-- /course-category -->
-		<?php endforeach; ?>
-	</div><!-- /container -->
-</div><!-- /course-list-section -->
+<?php $this->renderPartial('//course/_courseList', array(
+		'categories' => $categories,
+		'courses_in_categories' => $courses_in_categories,
+));?>
