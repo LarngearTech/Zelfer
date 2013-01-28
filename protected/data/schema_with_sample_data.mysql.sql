768c52bcd81c19d27144b79b4805a49e110c7efc
Supasate Choochaisri <supasate@larngeartech.com>
2013-01-28 15:10:33 +0700

    Add user group and subgroup to schema.

--- protected/data/schema_with_sample_data.mysql.sql
+++ protected/data/schema_with_sample_data.mysql.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 20, 2013 at 07:21 PM
--- Server version: 5.5.28
--- PHP Version: 5.3.10-1ubuntu3.4
+-- Generation Time: Jan 28, 2013 at 03:10 PM
+-- Server version: 5.5.29
+-- PHP Version: 5.3.10-1ubuntu3.5
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -85,7 +85,7 @@ CREATE TABLE IF NOT EXISTS `content` (
   `parent_id` int(11) NOT NULL,
   `type` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=13 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=54 ;
 
 --
 -- Dumping data for table `content`
@@ -95,15 +95,24 @@ INSERT INTO `content` (`id`, `name`, `course_id`, `order`, `parent_id`, `type`) 
 (1, 'รู้จักกับอัลกอริทึม', 1, 0, -1, 0),
 (2, 'อัลกอริทึมคืออะไร', 1, 1, 1, 2),
 (3, 'เกี่ยวกับคอร์สนี้', 1, 2, 1, 2),
-(4, 'การวิเคราะห์ประสิทธิภาพอัลกอริทึม', 1, 3, -1, 0),
-(5, 'สัญกรณ์โอใหญ่', 1, 4, 4, 2),
-(6, 'อัตราการเติบโตของฟังก์ชัน', 1, 5, 4, 1),
-(7, 'โครงสร้างข้อมูลพื้นฐาน', 1, 6, -1, 0),
-(8, 'อาเรย์และรายการเชื่อมโยง', 1, 7, 7, 1),
-(9, 'การเรียงลำดับและการค้นข้อมูล', 1, 8, 7, 1),
-(10, 'ลำดับย่อยร่วมกันที่ยาวที่สุด', 1, 10, 11, 1),
-(11, 'ไดนามิกโปรแกรมมิ่ง', 1, 9, -1, 0),
-(12, 'ทดสอบ', 2, 0, -1, 2);
+(4, 'การวิเคราะห์ประสิทธิภาพอัลกอริทึม', 1, 4, -1, 0),
+(5, 'สัญกรณ์โอใหญ่', 1, 5, 4, 2),
+(6, 'อัตราการเติบโตของฟังก์ชัน', 1, 6, 4, 1),
+(7, 'โครงสร้างข้อมูลพื้นฐาน', 1, 7, -1, 0),
+(8, 'อาเรย์และรายการเชื่อมโยง', 1, 8, 7, 1),
+(9, 'การเรียงลำดับและการค้นข้อมูล', 1, 9, 7, 1),
+(10, 'ลำดับย่อยร่วมกันที่ยาวที่สุด', 1, 11, 11, 1),
+(11, 'ไดนามิกโปรแกรมมิ่ง', 1, 10, -1, 0),
+(12, 'ทดสอบ', 2, 1, 13, 2),
+(13, 'Untitled Chapter', 2, 0, -1, 0),
+(42, 'Quiz', 1, 3, 1, 3),
+(39, 'Untitled Question', 1, 0, 14, 4),
+(41, 'Untitled Question', 1, 0, 40, 4),
+(48, 'qqq', 1, 0, 42, 4),
+(49, 'q2', 1, 1, 42, 4),
+(51, 'q3', 1, 2, 42, 4),
+(52, 'q4', 1, 3, 42, 4),
+(53, 'q5', 1, 4, 42, 4);
 
 -- --------------------------------------------------------
 
@@ -129,10 +138,10 @@ CREATE TABLE IF NOT EXISTS `course` (
 --
 
 INSERT INTO `course` (`id`, `name`, `short_description`, `long_description`, `category_id`, `thumbnail_url`, `intro_url`, `owner_id`, `status`) VALUES
-(1, 'Computer Algorithm', 'พื้นฐานของการออกแบบและวิเคราะห์อัลกอริทึม', 'เพื่อเป็นการเตรียมพร้อมสำหรับการแข่งขันคอมพิวเตอร์โอลิมปิก คอร์สนี้จะสอนให้คุณได้รู้ถึงโครงสร้างข้อมูลพื้นฐานจนไปถึงการวิเคราะห์และการออกแบบอัลกอริทึมที่จำเป็นสำหรับการแข่งขัน', 1, '/course/thumbnails/50876716cbd00.png', '5086d9d929a8d', 1, 1),
-(2, 'Software as a Service', 'การออกแบบซอฟต์แวร์เชิงบริการ', 'เพื่อเป็นการเตรียมพร้อมสำหรับการแข่งขันคอมพิวเตอร์โอลิมปิก คอร์สนี้จะสอนให้คุณได้รู้ถึงโครงสร้างข้อมูลพื้นฐานจนไปถึงการวิเคราะห์และการออกแบบอัลกอริทึมที่จำเป็นสำหรับการแข่งขัน', 1, '/course/thumbnails/50875c5165fc1.jpg', '5087648d641e9', 2, 1),
-(3, 'Introduction to Mathematical Thinking', 'คอร์สเบื้องต้นสำหรับกระบวนการคิดแบบคณิตศาสตร์', 'เพื่อเป็นการเตรียมพร้อมสำหรับการแข่งขันคอมพิวเตอร์โอลิมปิก คอร์สนี้จะสอนให้คุณได้รู้ถึงโครงสร้างข้อมูลพื้นฐานจนไปถึงการวิเคราะห์และการออกแบบอัลกอริทึมที่จำเป็นสำหรับการแข่งขัน', 2, '/course/thumbnails/50876716cbd00.png', '5087642a04180', 2, 1),
-(4, 'Human Computer Interaction', 'การออกแบบการเชื่อมต่อระหว่างคอมพิวเตอร์และผู้ใช้งาน', 'เพื่อเป็นการเตรียมพร้อมสำหรับการแข่งขันคอมพิวเตอร์โอลิมปิก คอร์สนี้จะสอนให้คุณได้รู้ถึงโครงสร้างข้อมูลพื้นฐานจนไปถึงการวิเคราะห์และการออกแบบอัลกอริทึมที่จำเป็นสำหรับการแข่งขัน', 1, '/course/thumbnails/5087636faed98.png', '508763c01ded3', 1, 1),
+(1, 'Computer Algorithm', 'พื้นฐานของการออกแบบและวิเคราะห์อัลกอริทึม', 'เพื่อเป็นการเตรียมพร้อมสำหรับการแข่งขันคอมพิวเตอร์โอลิมปิก คอร์สนี้จะสอนให้คุณได้รู้ถึงโครงสร้างข้อมูลพื้นฐานจนไปถึงการวิเคราะห์และการออกแบบอัลกอริทึมที่จำเป็นสำหรับการแข่งขัน', 1, '/zelfer/course/thumbnails/50ec13873d100.png', '5086d9d929a8d', 1, 1),
+(2, 'Software as a Service', 'การออกแบบซอฟต์แวร์เชิงบริการ', 'เพื่อเป็นการเตรียมพร้อมสำหรับการแข่งขันคอมพิวเตอร์โอลิมปิก คอร์สนี้จะสอนให้คุณได้รู้ถึงโครงสร้างข้อมูลพื้นฐานจนไปถึงการวิเคราะห์และการออกแบบอัลกอริทึมที่จำเป็นสำหรับการแข่งขัน', 1, '/zelfer/course/thumbnails/50875c5165fc1.jpg', '5087648d641e9', 2, 1),
+(3, 'Introduction to Mathematical Thinking', 'คอร์สเบื้องต้นสำหรับกระบวนการคิดแบบคณิตศาสตร์', 'เพื่อเป็นการเตรียมพร้อมสำหรับการแข่งขันคอมพิวเตอร์โอลิมปิก คอร์สนี้จะสอนให้คุณได้รู้ถึงโครงสร้างข้อมูลพื้นฐานจนไปถึงการวิเคราะห์และการออกแบบอัลกอริทึมที่จำเป็นสำหรับการแข่งขัน', 2, '/zelfer/course/thumbnails/50876716cbd00.png', '5087642a04180', 2, 1),
+(4, 'Human Computer Interaction', 'การออกแบบการเชื่อมต่อระหว่างคอมพิวเตอร์และผู้ใช้งาน', 'เพื่อเป็นการเตรียมพร้อมสำหรับการแข่งขันคอมพิวเตอร์โอลิมปิก คอร์สนี้จะสอนให้คุณได้รู้ถึงโครงสร้างข้อมูลพื้นฐานจนไปถึงการวิเคราะห์และการออกแบบอัลกอริทึมที่จำเป็นสำหรับการแข่งขัน', 1, '/zelfer/course/thumbnails/5087636faed98.png', '508763c01ded3', 1, 1),
 (5, 'Introduction to Logic', 'ตรรกศาสตร์เบื้องต้น', 'เพื่อเป็นการเตรียมพร้อมสำหรับการแข่งขันคอมพิวเตอร์โอลิมปิก คอร์สนี้จะสอนให้คุณได้รู้ถึงโครงสร้างข้อมูลพื้นฐานจนไปถึงการวิเคราะห์และการออกแบบอัลกอริทึมที่จำเป็นสำหรับการแข่งขัน', 2, '/course/thumbnails/50876754bc4db.png', NULL, 2, 1),
 (6, 'ทดสอบการใช้งาน', 'สำหรับทดสอบการใช้งาน', 'คอร์สนี้สร้างมาเพื่อทดสอบการใช้งานระบบ', 2, NULL, NULL, 2, 0),
 (7, 'test test tes', 'test', 'test', 1, NULL, NULL, 2, 0),
@@ -303,8 +312,58 @@ CREATE TABLE IF NOT EXISTS `user` (
 INSERT INTO `user` (`id`, `email`, `password`, `fullname`, `profile_image_url`, `career`, `description`, `role`, `status`) VALUES
 (1, 'admin@zelfer.com', '$2a$08$AlQ93iTsZOZ6vQv7CRiwheS3gh16gRw5e2Ngw6q5qVc/PkVPQ0DHC', 'Tim Roughgarden', '/zelfer/user/1.jpg', 'Operations Manager at Larngear Technology', 'Supasate Choochaisri is a co-founder and managing director of Larngear Technology Co., Ltd. His company has\r\nwon several regional and international awards. He receives B. Eng., M. Eng., and Ph.D. in Computer Engineering, from Chulalongkorn University. \r\n\r\nCurrently, he has received a grant CP CU\r\nAcademic Excellence Scholarship. His research interests include various topics in augmented reality and ubiquitous computing with emphasis\r\non wireless sensor network, mobile computing, and distributed algorithms.\r\n', 1, 1),
 (2, 'demo@zelfer.com', '$2a$08$gA137nB8.aZqbRBNjbur3OXWlyf7zv8MWnzFVzge06IPTNnJKFXdi', 'Shane Wesley', '/zelfer/user/2.jpg', 'Tester Account at Larngear Technology', 'This is only an account for system testing.', 2, 1),
-(3, 'test1@test.com', '$2a$08$I2Clo09T8bNf0FoDtDfj4uvQnlCUcYkbhgql.0tFlfuRy9ioZKQQe', '/zelfer/user/3.jpg', '', '', '', 2, 1),
-(4, 'test2@test.com', '$2a$08$xWAk15KGjyeKRMRqwxmSTuMXvj3Yy256Laeoo7RVjxPH6UzW3dx.a', 'test2', '', '', '', 2, 1);
+(3, 'system@zelfer.com', '$2a$08$hQQfmgTMYKS9UHRiRzDa7.6SyAozbGIJcc9Uhy8IqcEkvVZOXgpiq', 'System Administrator', 'default.gif', '', '', 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_group`
+--
+
+CREATE TABLE IF NOT EXISTS `user_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `user_group`
+--
+
+INSERT INTO `user_group` (`id`, `name`) VALUES
+(1, '2555'),
+(2, '2556');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_subgroup`
+--
+
+CREATE TABLE IF NOT EXISTS `user_subgroup` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=13 ;
+
+--
+-- Dumping data for table `user_subgroup`
+--
+
+INSERT INTO `user_subgroup` (`id`, `group`, `name`) VALUES
+(1, 1, 'ห้อง 1'),
+(2, 1, 'ห้อง 2'),
+(3, 1, 'ห้อง 3'),
+(4, 1, 'ห้อง 4'),
+(5, 1, 'ห้อง 5'),
+(6, 1, 'ห้อง 6'),
+(7, 2, 'ห้อง 1'),
+(8, 2, 'ห้อง 2'),
+(9, 2, 'ห้อง 3'),
+(10, 2, 'ห้อง 4'),
+(11, 2, 'ห้อง 5'),
+(12, 2, 'ห้อง 6');
 
 -- --------------------------------------------------------
 
