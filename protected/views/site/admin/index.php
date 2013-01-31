239a7cc2f0d878a1849b38b3ba2f090449e92470
Supasate Choochaisri <supasate@larngeartech.com>
2013-01-31 11:28:25 +0700

    Align code.

--- protected/views/site/admin/index.php
+++ protected/views/site/admin/index.php
@@ -1,4 +1,4 @@
-		<?php 
+<?php 
 
 $this->pageTitle=Yii::app()->name; 
 
@@ -9,83 +9,83 @@ Yii::app()->clientScript->registerScript(
         'toggle-taking-teaching',
         '$(function() {
 			$("#student-btn").on("click", function(event) {
-	                	$("#student-list").show();
-	                	$("#teacher-list").hide();
-	        	});
-	        	$("#teacher-btn").on("click", function(event) {
-	                	$("#student-list").hide();
-	                	$("#teacher-list").show();
-	        	});
-			});
-			function addUserGroup() 
+                	$("#student-list").show();
+                	$("#teacher-list").hide();
+        	});
+        	$("#teacher-btn").on("click", function(event) {
+                	$("#student-list").hide();
+                	$("#teacher-list").show();
+        	});
+		});
+		function addUserGroup() 
+		{
+			if ($("#txtUserGroupName").val() == "")
 			{
-				if ($("#txtUserGroupName").val() == "")
-				{
-					alert("Please specify group name.");
-				}
-				else
-				{
-					$.ajax({
-						url: "'.Yii::app()->createUrl('userGroup/create').'",
-						type: "POST",
-						dataType: "html",
-						data: {
-							"UserGroup[name]": $("#txtUserGroupName").val()
-						},
-						success: function(html) {
-							$("#group-list").html(html);
-						}
-					});
-				}
+				alert("Please specify group name.");
 			}
-			function addStudent(){
-				if ($("#txtStudentFullName").val() == ""
-				|| $("#txtStudentEmail").val() == "")
-				{
-					alert("Not enough information to create new account");
-				}
-				else
-				{
-					$.ajax({
-						url:"'.Yii::app()->createUrl('user/add').'",
-						type:"POST",
-						dataType:"html",
-						data:{
-							fullname:$("#txtStudentFullName").val(),
-							email:$("#txtStudentEmail").val(),
-							role:"2"
-						},
-						success:function(html){
-							$("#student-list").html(html);
-						}
-					});
-				}
+			else
+			{
+				$.ajax({
+					url: "'.Yii::app()->createUrl('userGroup/create').'",
+					type: "POST",
+					dataType: "html",
+					data: {
+						"UserGroup[name]": $("#txtUserGroupName").val()
+					},
+					success: function(html) {
+						$("#group-list").html(html);
+					}
+				});
 			}
-			function addTeacher(){
-				if ($("#txtTeacherFullName").val() == ""
-				|| $("#txtTeacherEmail").val() == "")
-				{
-					alert("Not enough information to create new account");
-				}
-				else
-				{
-					$.ajax({
-						url:"'.Yii::app()->createUrl('user/add').'",
-						type:"POST",
-						dataType:"html",
-						data:{
-							fullname:$("#txtTeacherFullName").val(),
-							email:$("#txtTeacherEmail").val(),
-							role:"1"
-						},
-						success:function(html){
-							$("#teacher-list").html(html);
-						}
-					});
-				}
-			}'	
-		, CClientScript::POS_END
-);?>
+		}
+		function addStudent(){
+			if ($("#txtStudentFullName").val() == ""
+			|| $("#txtStudentEmail").val() == "")
+			{
+				alert("Not enough information to create new account");
+			}
+			else
+			{
+				$.ajax({
+					url:"'.Yii::app()->createUrl('user/add').'",
+					type:"POST",
+					dataType:"html",
+					data:{
+						fullname:$("#txtStudentFullName").val(),
+						email:$("#txtStudentEmail").val(),
+						role:"2"
+					},
+					success:function(html){
+						$("#student-list").html(html);
+					}
+				});
+			}
+		}
+		function addTeacher(){
+			if ($("#txtTeacherFullName").val() == ""
+			|| $("#txtTeacherEmail").val() == "")
+			{
+				alert("Not enough information to create new account");
+			}
+			else
+			{
+				$.ajax({
+					url:"'.Yii::app()->createUrl('user/add').'",
+					type:"POST",
+					dataType:"html",
+					data:{
+						fullname:$("#txtTeacherFullName").val(),
+						email:$("#txtTeacherEmail").val(),
+						role:"1"
+					},
+					success:function(html){
+						$("#teacher-list").html(html);
+					}
+				});
+			}
+		}'	
+		, CClientScript::POS_END);
+?>
 <div class="container">
 	<div class="row">
 		<div class="stack-wrapper span3">
